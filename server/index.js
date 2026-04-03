cb75da60de78020f49eb1ef4f81fa5b06f7a42bc
chaheein123 <chaheein@gmail.com>
2026-04-02 22:02:43 -0700

    testing

--- server/index.js
+++ server/index.js
@@ -18,7 +18,7 @@ const pgClient = new Pool({
   password: keys.pgPassword,
   port: keys.pgPort,
   ssl:
-    process.env.NODE_ENV !== 'productionsss'
+    process.env.NODE_ENV !== 'production'
       ? false
       : { rejectUnauthorized: false },
 });
